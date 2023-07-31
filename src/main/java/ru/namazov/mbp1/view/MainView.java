1e51b21e0fe92e2234c037bafee0f752d40bf2b9
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-07-31 18:50:12 +0300

    убрал импорты

--- src/main/java/ru/namazov/mbp1/view/MainView.java
+++ src/main/java/ru/namazov/mbp1/view/MainView.java
@@ -4,15 +4,7 @@
 
 package ru.namazov.mbp1.view;
 
-import java.util.List;
-
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.ParentLayout;
 import com.vaadin.flow.router.Route;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Route("admin")
 public class MainView extends BaseView {
