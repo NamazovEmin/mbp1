5940b3787b96cd4c39a5a609076de8bbab164e32
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-08-06 01:36:43 +0300

    Добавил временные настройки секурити для теста

---
+++ src/main/java/ru/namazov/mbp1/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023, TopS BI LLC. All rights reserved.
+ * http://www.topsbi.ru
+ */
+
+package ru.namazov.mbp1.security;
+
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import ru.namazov.mbp1.login.view.LoginView;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends VaadinWebSecurity {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        setLoginView(http, LoginView.class);
+    }
+
+    @Bean
+    UserDetailsManager userDetailsManager(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("marcus")
+                        .password("{noop}marcus")
+                        .roles("USER").build()
+        );
+    }
+}
