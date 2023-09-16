90d61fb9fa582ae6676383f0156da970217a1b67
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-09-16 23:34:55 +0300

    Доделал полностью Заказ

---
+++ src/main/java/ru/namazov/mbp1/view/MainView.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) . Namazov Emin. All rights reserved.
+ */
+
+package ru.namazov.mbp1.view;
+
+import com.vaadin.flow.router.Route;
+
+@Route("admin")
+public class MainView extends BaseView {
+
+    public MainView() {
+        super();
+    }
+}
